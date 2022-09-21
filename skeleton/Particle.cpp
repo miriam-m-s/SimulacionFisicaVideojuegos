85d6a78d886e4400b4c33cc0c4184dd0282a6c6a
miriam-m-s <81752837+miriam-m-s@users.noreply.github.com>
2022-09-21 09:45:01 +0200

    colores

--- skeleton/Particle.cpp
+++ skeleton/Particle.cpp
@@ -17,12 +17,61 @@ Particle::~Particle()
 void Particle::integrate(double t)
 {
 	pose.p = pose.p + vel * t;
+
 	
-	if (color.y >= 0.90) {
-		color.y = 0;
+	cambiarcolor();
+
+	
+}
+
+void Particle::cambiarcolor()
+{
+	if (i == 0) {
+		if (!cambio) {
+			color.x -= 0.0005;
+			if (color.x <= 0.5) {
+				color.x = 0; cambio = true;
+			}
+		}
+		else {
+			color.x += 0.0005;
+			if (color.x >= 0.99) {
+				color.x = 1; cambio = false;
+				i++;
+
+			}
+		}
+	}
+	else if (i == 1) {
+		if (!cambio) {
+			color.y -= 0.0005;
+			if (color.y <= 0.5) {
+				color.y = 0; cambio = true;
+			}
+		}
+		else {
+			color.y += 0.0005;
+			if (color.y >= 0.99) {
+				color.y = 1; cambio = false;
+				i++;
+			}
+		}
 	}
 	else {
-		color.y += 0.01;
+		if (!cambio) {
+			color.z -= 0.0005;
+			if (color.z <= 0.5) {
+				color.z = 0; cambio = true;
+			}
+		}
+		else {
+			color.z += 0.0005;
+			if (color.z >= 0.99) {
+				color.z = 1; cambio = false;
+				i = 0;
+			}
+		}
 	}
 	renderitem->color = color;
+
 }
--- skeleton/Particle.h
+++ skeleton/Particle.h
@@ -7,10 +7,14 @@ public:
 	Particle(Vector3 Pos, Vector3 Vel);
 	~Particle();
 	void integrate(double t);
+	void cambiarcolor();
 private:
 	Vector3 vel;
 	physx::PxTransform pose;
 	RenderItem* renderitem;
 	Vector4 color;
+	bool cambio = false;
+	bool changecolor = false;
+	int i = 0;
 };
 
